2e33a4b70b68f4516652e2474469e0f0b7b1cb3c
Billy <billy@localhost.localdomain>
2026-07-13 15:08:38 +0300

    Fix static files: use StaticFilesStorage instead of manifest storage

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
